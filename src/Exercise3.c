61c1b5912590991da4aef699788d4c15da0254bb
Nguyễn Trần Duy Thức ITITIU20318 <thuccau1072002@gmail.com>
2021-10-18 18:01:40 +0700

    bai3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -14,8 +14,46 @@ Ex:
 
 void Ex3(int in_arr[], int n){
 	//Your codes here
-	
+    int i,j;
+
+    for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (in_arr[j] > in_arr[i])
+			{
+				int tmp = in_arr[i];
+				in_arr[i] = in_arr[j];
+				in_arr[j] = tmp;
+			}
+		}
+	}
+	printf("Increasing");
+	for (int i = 0; i < n; i++)
+	{
+		printf(" %d", in_arr[i]);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (in_arr[j] < in_arr[i])
+			{
+				int tmp = in_arr[i];
+				in_arr[i] = in_arr[j];
+				in_arr[j] = tmp;
+			}
+		}
+	}
+	printf(" Decreasing");
+	for (int i = 0; i < n; i++)
+	{
+		printf(" %d ", in_arr[i]);
+	}
 }
+	
+	
+
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
