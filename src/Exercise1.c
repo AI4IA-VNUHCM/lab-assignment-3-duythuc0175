a936be4605830c9c763db0856a3a67e6437a483a
Nguyễn Trần Duy Thức ITITIU20318 <thuccau1072002@gmail.com>
2021-10-18 14:40:53 +0700

    update bai1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -17,12 +17,12 @@ void Ex1(int arr[], int n){
     int i,j;
 	int sym=1;
 	for(i=0,j=n-1;(i <= floor((n-1)/2)) && (j >= ceil((n-1)/2));i++,j--)
-	{
+	
 		if(sym==0)
 		printf(" asymmetric");
 		else
 		printf(" symmetric");
-	}
+	
 }
 
 int main(int argc, char *argv[]) {
